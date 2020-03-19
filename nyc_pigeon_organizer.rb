ac8feb1f7db2f6ad49e1b6713c114bca15f714e6
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-19 20:39:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,7 @@ def nyc_pigeon_organizer(data) binding.pry
    value.each do |new_value, names|
      names.each do |name|
     end 
+    if !pigeons[name]
        if !pigeons[name, key]
          pigeons[name, key] = []
 
